ff9e1c90e4355c7da54818635f07dd28bc246430
NikDoe <nikd0e@yandex.ru>
2024-10-19 18:15:29 +0300

    feat: create login page for user authentication
    Added a basic login page to authenticate users in the auth toolkit
    project.

---
+++ components/auth/LoginButton.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
+interface LoginButtonProps {
+	asChild?: boolean;
+	children: React.ReactNode;
+	mode?: 'modal' | 'redirect';
+}
+
+function LoginButton({
+	asChild,
+	children,
+	mode = 'redirect'
+}: LoginButtonProps) {
+	const router = useRouter();
+
+	const onClick = () => {
+		router.push('/auth/login')
+	}
+
+	if (mode === 'modal') {
+		return (
+			<span>
+				TODO: создать модалку
+			</span>
+		)
+	}
+
+	return (
+		<span onClick={onClick} className="cursor-pointer">
+			{children}
+		</span>
+	)
+}
+
+export default LoginButton
