d83c5cd4837a693451393536891e4b5e14c6a31d
NikDoe <nikd0e@yandex.ru>
2024-10-21 20:38:41 +0300

    feat: add user creation on registration w/ validation
    Implemented functionality to create a new user upon successful
    registration. Validates form fields and ensures the email is unique
    before adding the user to the database.

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server'
 
 import { z } from 'zod'
+import bcrypt from 'bcrypt'
 import { RegisterSchema } from '@/schemas'
+import { getUserByEmail } from '@/data/user'
+import { db } from '@/lib/db'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validatedFields = RegisterSchema.safeParse(values)
@@ -10,5 +13,20 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 		return { error: "невалидные данные" }
 	}
 
-	return { success: 'проверьте почту' }
+	const { email, name, password } = validatedFields.data;
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const user = await getUserByEmail(email);
+
+	if (user) return { error: "Пользователь с таким email уже существует" };
+
+	await db.user.create({
+		data: {
+			email,
+			name,
+			password: hashedPassword,
+		}
+	})
+
+	return { success: 'Пользователь создан' };
 }
