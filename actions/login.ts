f003003c5be7dd58e4968ec040389c760e2e60a0
NikDoe <nikd0e@yandex.ru>
2024-10-22 17:16:05 +0300

    feat: implement authentication using next-auth
    Added authentication setup using the next-auth library.
    - Implemented providers for credentials-based authentication.
    - Set up login validation using zod and bcrypt for password verification.
    - Added routes for public access and authentication-related routes.
    - Configured redirection for users based on their authentication status.
    - Created handlers for authentication API.
    
    This implementation provides secure access to protected routes and
    session management using JWT.

--- actions/login.ts
+++ actions/login.ts
@@ -1,7 +1,10 @@
 'use server'
+import { AuthError } from 'next-auth'
 
 import { z } from 'zod'
 import { LoginSchema } from '@/schemas'
+import { signIn } from '@/auth'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 	const validatedFields = LoginSchema.safeParse(values)
@@ -10,5 +13,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 		return { error: "невалидные данные" }
 	}
 
-	return { success: 'проверьте почту' }
+	const { email, password } = validatedFields.data;
+
+	try {
+		await signIn('credentials', {
+			email,
+			password,
+			redirectTo: DEFAULT_LOGIN_REDIRECT,
+		})
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case "CredentialsSignin":
+					return { error: 'Неправильные email или пароль' }
+				default:
+					return { error: 'Что-то пошло не так при логине' }
+			}
+		}
+
+		throw error;
+	}
 }
---
+++ routes.ts
@@ -0,0 +1,36 @@
+/**
+ * Publicly accessible routes in the application.
+ * These routes do not require authentication.
+ * 
+ * @type {string[]}
+ */
+export const publicRoutes = [
+	'/',
+];
+
+/**
+ * Routes related to authentication.
+ * These routes will redirect logged in users to /settings.
+ * 
+ * @type {string[]}
+ */
+export const authRoutes = [
+	'/auth/login',
+	'/auth/register',
+];
+
+/**
+ * Prefix for authentication-related API endpoints.
+ * This is used as the base path for auth API routes.
+ * 
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * Default redirect path after a successful login.
+ * Users will be redirected to this path upon logging in.
+ * 
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
