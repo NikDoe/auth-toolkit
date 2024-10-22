bf6671924e8b142a53cc11018847a4a8eece092f
NikDoe <nikd0e@yandex.ru>
2024-10-22 21:51:31 +0300

    feat: added role based auth

--- auth.ts
+++ auth.ts
@@ -1,10 +1,47 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { UserRole } from "@prisma/client"
 import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
 
+import { getUserById } from "@/data/user"
+
+type extendedUser = DefaultSession["user"] & {
+	role: UserRole;
+}
+
+declare module "next-auth" {
+	interface Session {
+		user: extendedUser,
+	}
+}
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
+	callbacks: {
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole;
+			}
+
+			return session
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token
+
+			const existingUser = await getUserById(token.sub);
+
+			if (!existingUser) return token;
+
+			token.role = existingUser.role;
+
+			return token
+		}
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: "jwt" },
 	...authConfig,
