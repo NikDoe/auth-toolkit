5df9243e9033e28f647e297ef2a33ee46e0c25f1
NikDoe <nikd0e@yandex.ru>
2024-10-24 22:57:43 +0300

    chore: upgrade Next.js to version 15
    Updated Next.js to the latest version (15) to leverage new features
    and improvements.
    Try to fix useSearchParams Boundary error.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,9 @@
 import { LoginForm } from "@/components/auth/LoginForm"
-import { Suspense } from 'react'
 
 function LoginPage() {
 	return (
 		<div className="h-full w-full bg-lime-300 flex items-center justify-center">
-			<Suspense>
-				<LoginForm />
-			</Suspense>
+			<LoginForm />
 		</div>
 	)
 }
--- app/auth/new-verification/page.tsx
+++ app/auth/new-verification/page.tsx
@@ -1,12 +1,9 @@
 import NewVerificationForm from "@/components/auth/NewVerificationForm"
-import { Suspense } from 'react'
 
 function NewVerificationPage() {
 	return (
 		<div className="h-full w-full bg-lime-300 flex items-center justify-center">
-			<Suspense>
-				<NewVerificationForm />
-			</Suspense>
+			<NewVerificationForm />
 		</div>
 
 	)
